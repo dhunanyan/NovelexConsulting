adb581cd0218ed8c1b915afbd573ced2e34509a4
poprostu-armani <davit.hunanyan@student.up.krakow.pl>
2021-08-19 07:28:30 +0200

    js-title-line-animation

--- purpose/js/script.js
+++ purpose/js/script.js
@@ -76,3 +76,84 @@ document.querySelectorAll('a.yakor').forEach(link => {
         });
     });
 })
+
+//MAGIC SCROLL
+
+var tl = new TimelineMax({ onUpdate: updatePercentage });
+var tl1 = new TimelineMax({ onUpdate: updatePercentage });
+var tl2 = new TimelineMax({ onUpdate: updatePercentage });
+var tl3 = new TimelineMax({ onUpdate: updatePercentage });
+var tl4 = new TimelineMax({ onUpdate: updatePercentage });
+const controller = new ScrollMagic.Controller();
+
+tl.from(".purpose-title", 0.5, { x: 200, opacity: 0 });
+tl.from(".purpose-line", 1, { width: 0 }, "=-.5");
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: "#purpose",
+    triggerHook: "onEnter",
+    duration: "100%"
+})
+    .setPin("#purposes")
+    .setTween(tl1)
+    .addTo(controller);
+
+
+tl1.from(".value__title", 0.5, { x: 200, opacity: 0 });
+tl1.from(".value__line", 1, { width: 0 }, "=-.5");
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: "#value",
+    triggerHook: "onEnter",
+    duration: "100%"
+})
+    .setPin("#values")
+    .setTween(tl1)
+    .addTo(controller);
+
+tl2.from(".mission__title", 0.5, { x: 200, opacity: 0 });
+tl2.from(".mission__line", 1, { width: 0 }, "=-.5");
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: "#mission",
+    triggerHook: "onEnter",
+    duration: "100%"
+})
+    .setPin("#missions")
+    .setTween(tl2)
+    .addTo(controller);
+
+tl3.from(".strategy__title", 0.5, { x: 200, opacity: 0 });
+tl3.from(".strategy__line", 1, { width: 0 }, "=-.1");
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: "#strategy",
+    triggerHook: "onEnter",
+    duration: "100%"
+})
+    .setPin("#strategys")
+    .setTween(tl3)
+    .addTo(controller);
+
+tl4.from(".partners__title", 0.5, { x: 200, opacity: 0 });
+tl4.from(".partners__line", 1, { width: 0 }, "=-.1");
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: "#partners",
+    triggerHook: "onEnter",
+    duration: "100%"
+})
+    .setPin("#partner")
+    .setTween(tl4)
+    .addTo(controller);
+
+
+function updatePercentage() {
+    //percent.innerHTML = (tl.progress() *100 ).toFixed();
+    tl.progress();
+    tl1.progress();
+    tl2.progress();
+    tl3.progress();
+    tl4.progress();
+    console.log(tl.progress());
+}
