106a907eef97cf6da84902edd4bbfac2eb9830fd
poprostu-armani <85347925+poprostu-armani@users.noreply.github.com>
2021-09-10 14:55:25 +0200

    cards-js-fix

--- js/script.js
+++ js/script.js
@@ -1,200 +1,205 @@
-"use strict"
+"use strict";
 
 const isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (
-			isMobile.Android() ||
-			isMobile.BlackBerry() ||
-			isMobile.iOS() ||
-			isMobile.Opera() ||
-			isMobile.Windows());
-	}
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
 };
 
 //DROPDOWN
 
 if (isMobile.any()) {
-	document.body.classList.add('_touch');
+  document.body.classList.add("_touch");
 
-	let menuArrows = document.querySelectorAll('.menu__arrow');
-	if (menuArrows.length > 0) {
-		for (let index = 0; index < menuArrows.length; index++) {
-			const menuArrow = menuArrows[index];
-			menuArrow.addEventListener("click", function (e) {
-				menuArrow.parentElement.classList.toggle('_active');
-			});
-		}
-	}
-
+  let menuArrows = document.querySelectorAll(".menu__arrow");
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle("_active");
+      });
+    }
+  }
 } else {
-	document.body.classList.add('_pc');
+  document.body.classList.add("_pc");
 }
 
 //MENU BURGER
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
 if (iconMenu) {
-	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-	});
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
 }
 
 //SCROLLBY
 
-document.querySelectorAll('a.yakor').forEach(link => {
-	link.addEventListener('click', function (e) {
-		e.preventDefault();
+document.querySelectorAll("a.yakor").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-		const href = this.getAttribute('href').substring(1);
+    const href = this.getAttribute("href").substring(1);
 
-		const scrollTarget = document.getElementById(href);
+    const scrollTarget = document.getElementById(href);
 
-		const topOffset = document.querySelector('header').offsetHeight;
-		const elementPosition = scrollTarget.getBoundingClientRect().top;
-		const offsetPosition = elementPosition - topOffset;
+    const topOffset = document.querySelector("header").offsetHeight;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
 
-		window.scrollBy({
-			top: offsetPosition,
-			behavior: 'smooth'
-		});
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
 
-		if (iconMenu.classList.contains('_active')) {
-			document.body.classList.remove('_lock');
-			iconMenu.classList.remove('_active');
-			menuBody.classList.remove('_active');
-		}
-	});
-})
+    if (iconMenu.classList.contains("_active")) {
+      document.body.classList.remove("_lock");
+      iconMenu.classList.remove("_active");
+      menuBody.classList.remove("_active");
+    }
+  });
+});
 
 //CARDS
 
-const cards = document.querySelectorAll('.card');
-if(cards.length > 0){
+const cards = document.querySelectorAll(".card");
+if (cards.length > 0) {
+  cards.forEach((card) => {
+    card.addEventListener("click", function (e) {
+      const currentCard = e.target;
 
-    cards.forEach(card => {
-        card.addEventListener("click", function(e){
-			const activeCards = document.querySelectorAll('.card._active');
+      currentCard.classList.toggle("_active");
 
-			if(activeCards.length > 0){
-				cards.forEach(card => {
-				card.classList.remove('_active');
-				});
-			}
-			e.target.classList.toggle('_active');
+      const activeCards = document.querySelectorAll(".card._active");
+      if (activeCards.length > 1) {
+        cards.forEach((activeCard) => {
+          activeCard.classList.remove("_active");
         });
-    })
+
+        currentCard.classList.toggle("_active");
+      }
+    });
+  });
 }
 
 //SLIDER
 
 $(document).ready(function () {
-	$('.slider').slick({
-		arrows: true,
-		dots: true,
-		adaptiveHeight: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		speed: 500,
-		easing: 'ease-out',
-		infinite: true,
-		initialSlide: 0,
-		autoplay: true,
-		autoplaySpeed: 2500,
-		pauseOnFocus: true,
-		pauseOnHover: true,
-		pauseOnDotsHover: true,
-		draggable: true,
-		swipe: true,
-		touchTreshold: 5,
-		touchMove: true,
-		waitForAnimate: false,
-		centerMode: false,
-		variableWidth: false,
-		rows: 1,
-		slidesPerRow: 1,
-		vertical: false,
-		verticalSwiping: false,
-		fade: false,
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1
-				}
-			},
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: "ease-out",
+    infinite: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchTreshold: 5,
+    touchMove: true,
+    waitForAnimate: false,
+    centerMode: false,
+    variableWidth: false,
+    rows: 1,
+    slidesPerRow: 1,
+    vertical: false,
+    verticalSwiping: false,
+    fade: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
 
-			{
-				breakpoint: 682,
-				settings: {
-					centerMode: false
-				}
-			}
-		],
-		mobileFirst: false
-	});
+      {
+        breakpoint: 682,
+        settings: {
+          centerMode: false,
+        },
+      },
+    ],
+    mobileFirst: false,
+  });
 });
 
 //CONTACT FORM
 
 window.addEventListener("DOMContentLoaded", function () {
-	// get the form elements defined in your form HTML above
+  // get the form elements defined in your form HTML above
 
-	var form = document.getElementById("contact__form");
-	//var button = document.getElementById("my-form-button");
-	var status = document.getElementById("status");
+  var form = document.getElementById("contact__form");
+  //var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
 
-	// Success and Error functions for after the form is submitted
+  // Success and Error functions for after the form is submitted
 
-	function success() {
-		form.reset();
-		status.classList.add("success");
-		status.innerHTML = "Thanks for your message! We will contact you as soon as possible.";
-	}
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML =
+      "Thanks for your message! We will contact you as soon as possible.";
+  }
 
-	function error() {
-		status.classList.add("error");
-		status.innerHTML = "Oops! There was a problem. Please try later or use other contact channels";
-	}
+  function error() {
+    status.classList.add("error");
+    status.innerHTML =
+      "Oops! There was a problem. Please try later or use other contact channels";
+  }
 
-	// handle the form submission event
+  // handle the form submission event
 
-	form.addEventListener("submit", function (ev) {
-		ev.preventDefault();
-		var data = new FormData(form);
-		ajax(form.method, form.action, data, success, error);
-	});
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
 });
 
 // helper function for sending an AJAX request
 
 function ajax(method, url, data, success, error) {
-	var xhr = new XMLHttpRequest();
-	xhr.open(method, url);
-	xhr.setRequestHeader("Accept", "application/json");
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState !== XMLHttpRequest.DONE) return;
-		if (xhr.status === 200) {
-			success(xhr.response, xhr.responseType);
-		} else {
-			error(xhr.status, xhr.response, xhr.responseType);
-		}
-	};
-	xhr.send(data);
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
--- services/js/script.js
+++ services/js/script.js
@@ -1,103 +1,106 @@
-"use strict"
+"use strict";
 
 const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows());
-    }
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
 };
 
 //DROPDOWN
 
 if (isMobile.any()) {
-    document.body.classList.add('_touch');
+  document.body.classList.add("_touch");
 
-    let menuArrows = document.querySelectorAll('.menu__arrow');
-    if (menuArrows.length > 0) {
-        for (let index = 0; index < menuArrows.length; index++) {
-            const menuArrow = menuArrows[index];
-            menuArrow.addEventListener("click", function (e) {
-                menuArrow.parentElement.classList.toggle('_active');
-            });
-        }
+  let menuArrows = document.querySelectorAll(".menu__arrow");
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle("_active");
+      });
     }
-
+  }
 } else {
-    document.body.classList.add('_pc');
+  document.body.classList.add("_pc");
 }
 
 //MENU BURGER
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
 if (iconMenu) {
-    iconMenu.addEventListener("click", function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
 }
 
 //SCROLLBY
 
-document.querySelectorAll('a.yakor').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll("a.yakor").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const href = this.getAttribute('href').substring(1);
+    const href = this.getAttribute("href").substring(1);
 
-        const scrollTarget = document.getElementById(href);
+    const scrollTarget = document.getElementById(href);
 
-        const topOffset = document.querySelector('header').offsetHeight;
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
+    const topOffset = document.querySelector("header").offsetHeight;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
 
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-
-        if (iconMenu.classList.contains('_active')) {
-            document.body.classList.remove('_lock');
-            iconMenu.classList.remove('_active');
-            menuBody.classList.remove('_active');
-        }
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
     });
-})
+
+    if (iconMenu.classList.contains("_active")) {
+      document.body.classList.remove("_lock");
+      iconMenu.classList.remove("_active");
+      menuBody.classList.remove("_active");
+    }
+  });
+});
 
 //CARDS
 
-const cards = document.querySelectorAll('.card');
-if(cards.length > 0){
+const cards = document.querySelectorAll(".card");
+if (cards.length > 0) {
+  cards.forEach((card) => {
+    card.addEventListener("click", function (e) {
+      const currentCard = e.target;
 
-    cards.forEach(card => {
-        card.addEventListener("click", function(e){
-			const activeCards = document.querySelectorAll('.card._active');
+      currentCard.classList.toggle("_active");
 
-			if(activeCards.length > 0){
-				cards.forEach(card => {
-				card.classList.remove('_active');
-				});
-			}
-			e.target.classList.toggle('_active');
+      const activeCards = document.querySelectorAll(".card._active");
+      if (activeCards.length > 1) {
+        cards.forEach((activeCard) => {
+          activeCard.classList.remove("_active");
         });
-    })
+
+        currentCard.classList.toggle("_active");
+      }
+    });
+  });
 }
